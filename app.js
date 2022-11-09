d66b4d02c5468c4fba44226e7dc44787df0a7f47
Cynthia Ketienyei <cynthiachptoo@gmail.com>
2022-11-09 12:00:29 +0300

    added a miidleware

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require ('express');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express()
 
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
 // Routes
 app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/index'));
 
 const PORT = process.env.PORT || 5000;
 
